2d4753ce339eb71c4caf5f26ed16bad4caa93a0d
krishna-creater <krish_n_a@outlook.com>
2022-08-20 00:14:28 +0530

    commit to heroku backend 1.0

---
+++ index.js
@@ -0,0 +1,28 @@
+const connectToMongo = require('./db');
+
+const cors = require('cors')
+
+const express = require('express')
+const app = express()
+
+require("dotenv").config();
+
+const port = 5000
+
+app.use(express.json())
+app.use(cors());
+
+connectToMongo();
+
+
+// Available Routes
+app.get("/",(req,res)=>{
+    res.send("hello from home route");
+})
+app.use("/api/auth",require('./routes/auth'));
+app.use('/api/notes', require("./routes/notes"));
+
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`iNotebook backend listening on port ${port}`);
+})
