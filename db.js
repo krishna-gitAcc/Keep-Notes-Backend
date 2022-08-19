7ac330fdc53b8078666e3cf7d6d63b2e430913ff
krishna-creater <krish_n_a@outlook.com>
2022-08-20 00:49:52 +0530

    commit to heroku backend 1.0

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://krish_n_a:Kris@2001@inotebook.sfkjoxd.mongodb.net/test";
+const mongoURI = "mongodb+srv://krish_n_a:Kris@2001@inotebook.sfkjoxd.mongodb.net/?retryWrites=true&w=majority";
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURI, ()=>{
